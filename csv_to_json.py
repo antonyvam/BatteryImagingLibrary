2886322d407cf89e4767d7e00638d15659f9bafe
rmdocherty <rmdocherty2@gmai.com>
2024-09-13 10:56:04 +0100

    updated json generation: now does floats, ints properly and returns lists of unique strings to filter by

--- csv_to_json.py
+++ csv_to_json.py
@@ -6,21 +6,51 @@ from json import dump
 # requires defining a primary gif
 
 imaging_types = ("lab", "xrd", "neutron", "synchotron")
-ScanTypes = Literal["full", "roi"]
+float_categories = ("exposure_time_s", "power", "voxel_size_microns", "magnification")
+int_categories = (
+    "battery_number" "projections",
+    "potential_kv",
+    "projections",
+    "pixel_binning",
+    "frame_binning",
+    "scan_time_min",
+    "frame_binning",
+    "acq_time_min",
+)
 
-data: dict[str, dict] = {}
+
+out: dict[str, dict] = {"data": {}, "unique_props": {}}
 for t in imaging_types:
-    data[t] = {}
+    out["data"][t] = {}
 
 
-def append_to_dict(obj: dict, key: str, entry: dict[str, str | float | int]) -> None:
+def append_to_dict(
+    obj: dict, key: str, entry: dict[str, str | float | int | list]
+) -> None:
     keys = obj.keys()
     if key in keys:
         n_existing_entries = len(obj[key].keys())
-        obj[key][str(n_existing_entries)] = entry
+        obj[key][n_existing_entries] = entry
     else:
         obj[key] = {}
-        obj[key]["0"] = entry
+        obj[key][0] = entry
+
+
+INVALID_STR = "N/A"
+
+
+def process_entry(val: str, header: str) -> str | float | int | list:
+    if val == "":
+        return INVALID_STR
+    elif header in float_categories:
+        return float(val)
+    elif header in int_categories:
+        return int(val)
+    elif header == "image_size":
+        dims = val.split("x")
+        return [int(d) for d in dims]
+    else:
+        return str(val)
 
 
 with open("test_sheet.csv", newline="") as csvfile:
@@ -28,17 +58,27 @@ with open("test_sheet.csv", newline="") as csvfile:
     headers: list[str] = next(
         reader,
     )
+    for h in headers:
+        out["unique_props"][h] = []
+
     for row in reader:
         imaging_type = row[1]
         assert imaging_type in imaging_types
         battery_type = row[2]
-        entry: dict[str, str | float | int] = {}
+        entry: dict[str, str | float | int | list] = {}
         for i, h in enumerate(headers):
+            to_add = process_entry(row[i], h)
+            if type(to_add) == str:
+                is_new = to_add not in out["unique_props"][h]
+                is_valid = to_add != INVALID_STR
+                if is_new and is_valid:
+                    out["unique_props"][h].append(to_add)
             if i < 3:
                 continue
-            entry[h] = str(row[i])
-        append_to_dict(data[imaging_type], battery_type, entry)
-print(data)
+            else:
+                entry[h] = to_add
+        append_to_dict(out["data"][imaging_type], battery_type, entry)
+print(out)
 
 with open("frontend/src/assets/data.json", "w") as f:
-    dump(data, f, indent=2, sort_keys=False)
+    dump(out, f, indent=2, sort_keys=False)

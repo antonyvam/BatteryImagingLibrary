e76da84aa3380ef1e6ba66a0cbf65a9fc662733a
rmdocherty <rmdocherty2@gmai.com>
2025-08-23 19:59:16 +0100

    added search term

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useContext} from "react";
 import {InputGroup, FormControl, DropdownButton, Dropdown, Button} from "react-bootstrap";
+import AppContext from "../interfaces/types";
 
 interface SearchBarProps {
     variant?: string;
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({variant = "outline-secondary"}) => {
+    const {
+        searchText: [searchText, setSearchText]
+    } = useContext(AppContext)!;
+
+    const onSearchChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        const value = e.currentTarget.value;
+        setSearchText(value);
+        // console.log(value);
+    };
+
     return (
         <InputGroup size="lg">
-            <FormControl placeholder="Search datasets..." aria-label="Search" />
+            <FormControl
+                placeholder="Search datasets..."
+                aria-label="Search"
+                onChange={onSearchChange}
+            />
             <DropdownButton
                 variant={variant}
                 title="Filters"

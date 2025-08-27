5abce1c3f52b5cf06983024acfd88d140207a2b3
rmdocherty <rmdocherty2@gmai.com>
2025-08-27 18:55:25 +0100

    added video player for examples

--- frontend/src/components/ExampleCard.tsx
+++ frontend/src/components/ExampleCard.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useRef, useState, useEffect} from "react";
 import {Card, Button} from "react-bootstrap";
 import {Container, Row, Col} from "react-bootstrap";
 import {ExampleCardData, isMobile} from "../interfaces/types";
 import {renderModality} from "../interfaces/helpers";
+import VideoPlayer from "./VideoPlayer";
 
 const DUMMY_TEXT =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet.";
@@ -11,12 +12,12 @@ const exampleData: ExampleCardData[] = [
     {
         modality: "XRD_CT",
         text: DUMMY_TEXT,
-        imgPath: "assets/imgs/examples/XRD_CT.gif"
+        imgPath: "XRD_CT"
     },
     {
         modality: "LAB_MICRO_XCT",
         text: DUMMY_TEXT,
-        imgPath: ""
+        imgPath: "XCT"
     },
     {
         modality: "NEUTRON_CT",
@@ -31,12 +32,12 @@ const exampleData: ExampleCardData[] = [
     {
         modality: "EDS",
         text: DUMMY_TEXT,
-        imgPath: "assets/imgs/examples/EDS.gif"
+        imgPath: "EDS"
     },
     {
         modality: "EBSD",
         text: DUMMY_TEXT,
-        imgPath: "assets/imgs/examples/ebsd.gif"
+        imgPath: "ebsd"
     }
 ];
 
@@ -45,11 +46,33 @@ interface ExampleCardProps {
 }
 
 const ExampleCard: React.FC<ExampleCardProps> = ({cardData}) => {
+    const divRef = useRef<HTMLDivElement>(null);
+
     const handleSeeAll = () => {
         // Dummy function
     };
+
+    const [hover, setHover] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (hover) {
+            divRef.current!.style.zIndex = "12";
+            divRef.current!.style.outline = `1px solid #000000`;
+        } else {
+            divRef.current!.style.zIndex = "0";
+            divRef.current!.style.outline = `1px solid #ffffff`;
+        }
+    }, [hover]);
+
     return (
-        <Card className="mb-3 shadow" style={{minHeight: 120}}>
+        <Card
+            className="mb-3 shadow"
+            style={{minHeight: 120}}
+            onMouseEnter={(e) => setHover(true)}
+            onPointerEnter={(e) => setHover(true)}
+            onPointerLeave={(e) => setHover(false)}
+            onMouseLeave={(e) => setHover(false)}
+        >
             <div
                 style={{
                     display: "flex",
@@ -58,6 +81,7 @@ const ExampleCard: React.FC<ExampleCardProps> = ({cardData}) => {
                     flexDirection: isMobile() ? "column" : "row",
                     maxHeight: isMobile() ? 600 : 300
                 }}
+                ref={divRef}
             >
                 <Card.Body>
                     <Card.Title className="h4" style={{marginBottom: 0}}>
@@ -84,7 +108,8 @@ const ExampleCard: React.FC<ExampleCardProps> = ({cardData}) => {
 
                     <Card.Text>{cardData.text}</Card.Text>
                 </Card.Body>
-                <img
+                <VideoPlayer fname={cardData.imgPath} active={hover} />
+                {/* <img
                     src={cardData.imgPath}
                     style={{
                         objectFit: "scale-down",
@@ -93,7 +118,7 @@ const ExampleCard: React.FC<ExampleCardProps> = ({cardData}) => {
                         // height: "100%"
                         // height: "fill-parent"
                     }}
-                />
+                /> */}
             </div>
         </Card>
     );
